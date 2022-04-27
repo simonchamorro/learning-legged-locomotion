6ab0f5e0034edf0128e838e2742cd9026bfd4977
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2022-04-27 09:22:30 -0400

    fixing isaac gym experiments scripts

--- isaac-gym/experiments/reward_exps.sh
+++ isaac-gym/experiments/reward_exps.sh
@@ -1,22 +1,22 @@
 
 # Reward Ablations
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_action_rate
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_ang_vel_xy
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_collision
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_dof_acc
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_air_time
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_vel_z
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_torque
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_ang_vel
-python legged_gym/scripts/train.py --task=go1_flat_rew_no_lin_vel
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_action_rate
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_ang_vel_xy
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_collision
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_dof_acc
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_air_time
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_vel_z
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_torque
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_ang_vel
+python legged_gym/legged_gym/scripts/train.py --task=go1_flat_rew_no_lin_vel
 
-python legged_gym/scripts/train.py --task=go1_rew_no_action_rate
-python legged_gym/scripts/train.py --task=go1_rew_no_ang_vel_xy
-python legged_gym/scripts/train.py --task=go1_rew_no_collision
-python legged_gym/scripts/train.py --task=go1_rew_no_dof_acc
-python legged_gym/scripts/train.py --task=go1_rew_no_air_time
-python legged_gym/scripts/train.py --task=go1_rew_no_vel_z
-python legged_gym/scripts/train.py --task=go1_rew_no_torque
-python legged_gym/scripts/train.py --task=go1_rew_no_ang_vel
-python legged_gym/scripts/train.py --task=go1_rew_no_lin_vel
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_action_rate
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_ang_vel_xy
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_collision
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_dof_acc
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_air_time
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_vel_z
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_torque
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_ang_vel
+python legged_gym/legged_gym/scripts/train.py --task=go1_rew_no_lin_vel
 
